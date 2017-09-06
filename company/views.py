627551045e1d91c15405492baf43617a9e8ed78b
Andrew Valleteau <avallete@student.42.fr>
2017-09-06 15:06:03 +0200

    [FEAT] Add main page
    Add main page, with company listing.

--- company/views.py
+++ company/views.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from models import Company
+
 
 def index(request):
-	return render(request, 'index.html', {})
-
+    companies = Company.objects.all()
+    paginator = Paginator(companies, 10)
+    if request.method == 'POST' and request.POST.get('search'):
+        companies = companies.filter(name__icontains=request.POST.get('search'))
+        return render(request, 'index.html', {'data': companies})
+    else:
+        page = request.GET.get('page')
+        try:
+            companies = paginator.page(page)
+        except PageNotAnInteger:
+            companies = paginator.page(1)
+        except EmptyPage:
+            companies = paginator.page(paginator.num_pages)
+        return render(request, 'index.html', {'data': companies})
